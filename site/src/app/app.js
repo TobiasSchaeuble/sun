5037eaec10702ab25ad28190cd02bf6692839f1e
Tobias C. S. Schaeuble <schaeuble.tobias@googlemail.com>
2015-10-06 14:32:59 +0200

    Time experiments

--- site/src/app/app.js
+++ site/src/app/app.js
@@ -11,8 +11,7 @@
     /**
      * Define the module and its dependencies
      */
-    angular.
-    module('sun', []).
+    angular.module('sun', []).
     directive('topBarImage', function() {
         return function(scope, element, attrs) {
             element.css({
@@ -23,8 +22,14 @@
             });
         };
     });
-
-
+    // var app = angular.module('sun', [])
+    //     app.controller('topbartime', function ($scope, $filter) {
+    //         var date = new Date();
+    //         $scope.ddMMyyyy = $filter('date')(new Date(), 'dd/MM/yyyy');
+    //         $scope.ddMMMMyyyy = $filter('date')(new Date(), 'dd, MMMM yyyy');
+    //         $scope.HHmmss = $filter('date')(new Date(), 'HH:mm:ss');
+    //         $scope.hhmmsstt = $filter('date')(new Date(), 'hh:mm:ss a');
+    //     });
 }());
 
 // function Ctrl($scope)
